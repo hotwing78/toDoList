256750e89880eebbdb7e1768b8d98a6ff716f5ff
hotwing78 <dlwingo@gmail.com>
2016-08-25 03:47:55 -0400

    a few adjustments

--- main.js
+++ main.js
@@ -1,115 +1,117 @@
-$(document).ready(function(){
-  toDo.init();
+$(document).ready(function() {
+    toDo.init();
 })
 
 var toDo = {
-   url: 'http://tiny-tiny.herokuapp.com/collections/toDoListWingoA',
+    url: 'http://tiny-tiny.herokuapp.com/collections/toDoListWingoA',
 
-   listItems: [],//end listItems
-   doneItems: [],//end doneItems
+    listItems: [], //end listItems
+    doneItems: [], //end doneItems
 
-   init: function(){
-     toDo.listRender();
-     toDo.events();
-   },//end init
+    init: function() {
+        toDo.listRender();
+        toDo.events();
+    }, //end init
 
-   listRender: function() {
-     toDo.getListItems();
-   },//end listRender
+    listRender: function() {
+        toDo.getListItems();
+    }, //end listRender
 
-   events: function(){
+    events: function() {
 
-     //add to do item
-     $('.add').on('click', function(event){
-         event.preventDefault();
-         var newToDoItem = {
-           content: $('textarea').val(),
-          };
-          toDo.createListItem(newToDoItem);
-          newToDoItem._id = '12345678';
-          $('textarea').val('');
-          var htmlStr = toDo.htmlGenerator(toDoTemplates.toDoItemTemplate,newToDoItem)
-          $('.listItems').append(htmlStr);
+        //add to do item
+        $('form').on('submit', function(event) {
+                event.preventDefault();
+                if ($('input').val() !== '') {
+                    var newToDoItem = {
+                        content: $('input').val(),
+                    };
+                    toDo.createListItem(newToDoItem);
+                    newToDoItem._id = '12345678';
+                    $('input').val('');
+                    var htmlStr = toDo.htmlGenerator(toDoTemplates.toDoItemTemplate, newToDoItem)
+                    $('.listItems').append(htmlStr);
+                }
 
-      }),
+            }),
 
-      //delete item
-      $('.done').on('click',function(event){
-         event.preventDefault();
-         if ( $("div.listItems >.checkbox > input" ).is( ":checked" ) ) {
-         var itemId = $("div.listItems >.checkbox > input" ).parent().attr('data-id');
-         var text = $('.checkbox').text();
-         console.log(text);
-         console.log(itemId);
-         toDo.deleteListItem(itemId);
-       }
-      })//end delete item
+            //delete item
+            $('.done').on('click', function(event) {
+                event.preventDefault();
+                if ($("div.listItems >.checkbox > input").is(":checked")) {
+                    var itemId = $("div.listItems >.checkbox > input").parent().attr('data-id');
+                    var text = $('.checkbox').text();
+                    console.log(text);
+                    console.log(itemId);
+                    toDo.deleteListItem(itemId);
+                }
+            }) //end delete item
 
-    },//end of events
+    }, //end of events
 
 
-      createListItem: function(item) {
+    createListItem: function(item) {
         $.ajax({
             url: toDo.url,
             method: "POST",
             data: item,
             success: function(data) {
-              console.log("WE CREATED SOMETHING", data);
+                console.log("WE CREATED SOMETHING", data);
                 toDo.listItems.push(data);
                 $('.listItems').html('');
-                toDo.listItems.forEach(function(element,idx) {
-                  var htmlStr = toDo.htmlGenerator(toDoTemplates.toDoItemTemplate,element);
-                  $('.listItems').append(htmlStr)
-                    })
-                },
+                toDo.listItems.forEach(function(element, idx) {
+                    var htmlStr = toDo.htmlGenerator(toDoTemplates.toDoItemTemplate, element);
+                    $('.listItems').append(htmlStr)
+                })
+            },
             error: function(err) {
-              console.error("Damn it!", err);
+                console.error("Damn it!", err);
             }
-          })
-      },//end of createListItem
+        })
+    }, //end of createListItem
 
-      getListItems: function() {
+    getListItems: function() {
         $.ajax({
-          url: toDo.url,
-          method: "GET",
-          success: function(data) {
-            console.log("WE GOT SOMETHING", data);
-            $('.listItems').html("");
-            data.forEach(function(element,idx) {
-              var htmlStr = toDo.htmlGenerator(toDoTemplates.toDoItemTemplate,element);
-              $('.listItems').append(htmlStr)
-            });
-              toDo.listItems = data;
-          },
-          error: function(err) {
-            console.error("Damn it!", err);
-          }
+            url: toDo.url,
+            method: "GET",
+            success: function(data) {
+                console.log("WE GOT SOMETHING", data);
+                $('.listItems').html("");
+                data.forEach(function(element, idx) {
+                    var htmlStr = toDo.htmlGenerator(toDoTemplates.toDoItemTemplate, element);
+                    $('.listItems').append(htmlStr)
+                });
+                toDo.listItems = data;
+            },
+            error: function(err) {
+                console.error("Damn it!", err);
+            }
         });
-      },//end getListItems
+    }, //end getListItems
 
-      deleteListItem: function(itemId){
+    deleteListItem: function(itemId) {
         var deleteItem = toDo.url + '/' + itemId;
         $.ajax({
-          url: deleteItem,
-          method: "DELETE",
-          success: function(data){
-            toDo.doneItems.push(data);
-            console.log("Get that outta here!", data);
-            toDo.getListItems();
-          },
-          error: function(err){
-            console.error('Damn it!',err);
-          }
+            url: deleteItem,
+            method: "DELETE",
+            success: function(data) {
+                toDo.doneItems.push(data);
+                console.log("Get that outta here!", data);
+                toDo.getListItems();
+            },
+            error: function(err) {
+                console.error('Damn it!', err);
+            }
         });
-      },// end deleteListItems
+    }, // end deleteListItems
 
 
-   templification: function(template) {
-     return _.template(template);
-   },
+    templification: function(template) {
+        return _.template(template);
+    },
 
-    htmlGenerator: function(template,data) {
-      var tmpl = toDo.templification(template);
-      return tmpl(data);
+    htmlGenerator: function(template, data) {
+        var tmpl = toDo.templification(template);
+        return tmpl(data);
     }
-};//var toDo ends here
+}; //var toDo ends here
